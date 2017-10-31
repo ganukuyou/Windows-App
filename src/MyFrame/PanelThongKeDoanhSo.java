ab6092c4175ffdb0fdefd8db6d068da49a7fc985
LeTrongPhuong <ganukuyou@gmail.com>
2017-11-01 03:27:17 +0700

    Hoan Thanh Panel Thong Ke

--- src/MyFrame/PanelThongKeDoanhSo.java
+++ src/MyFrame/PanelThongKeDoanhSo.java
@@ -5,12 +5,22 @@
  */
 package MyFrame;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.layout.Border;
 import javax.swing.table.DefaultTableModel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -37,9 +47,21 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
                 
             current.add(Calendar.DAY_OF_MONTH, -1);
          }
+         
+         try 
+         {
+            NapDuLieuThongKeTheoNgayDenLuuDo();
+         } catch (ClassNotFoundException ex)
+         {
+            Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SQLException ex) 
+         {
+            Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
     
     
+    //Nạp dữ liệu thống kê đến JTable
     private void NapDuLieuThongKe(DataTable dt)
     {   
         DefaultTableModel tModel = (DefaultTableModel)tblDoanhSo.getModel();
@@ -60,6 +82,7 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
         }
     }
     
+    //Tìm kiếm dữ liệu thống kê theo ngày
     private DataTable TimThongKeTheoNgay(String date) throws ClassNotFoundException, SQLException
     {
         
@@ -75,6 +98,7 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
         return dt;
     }
     
+    //Tìm kiếm dữ liệu thống kê dựa vào tháng và năm
     private DataTable TimThongKeTheoThangVaNam(String month, String year) throws ClassNotFoundException, SQLException
     {
         String query = "select sl.modelid, sl.dacdiem, count(*) as soluongban, sl.tonkho from hoadon" + 
@@ -88,6 +112,7 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
         return dt;
     }
     
+    //Tìm Kiếm dữ liệu thống kê dựa vào năm
     private DataTable TimThongKeTheoNam(String year) throws ClassNotFoundException, SQLException
     {
         String query = "select sl.modelid, sl.dacdiem, count(*) as soluongban, sl.tonkho from hoadon" + 
@@ -100,6 +125,128 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
         
         return dt;
     }
+    
+    //Nạp dữ liệu thống kê theo năm đến lưu đồ
+    private void NapDuLieuThongKeTheoNamDenLuuDo() throws ClassNotFoundException, SQLException
+    {
+        String query = "select tk.year, count(*) as soluong from" + 
+                        " (select year(hoadon.ngayxuat) as year from chitiethd" + 
+                        " join hoadon on chitiethd.idhd = hoadon.idhd) tk group by tk.year;";
+        
+        DataTable dt = new DataTable("localhost", "clock", 2, query);
+        
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        
+        for(int i = 0; i < dt.getRowCount(); i++)
+        {
+            Object row[] = dt.getRow(i);
+            Number n = new Long((long)row[1]);
+            dataset.addValue(n, "soluong", row[0].toString());
+        }
+        
+        //Khởi tạo lưu đồ
+        JFreeChart chart = ChartFactory.createBarChart("Biểu Đồ Doanh Thu", "", "", dataset, PlotOrientation.VERTICAL, false, false, false);
+        CategoryPlot catPlot = chart.getCategoryPlot();
+        catPlot.setRangeGridlinePaint(Color.BLACK);
+        
+        //Đưa lưu đồ vào panel
+        ChartPanel chartpanel = new ChartPanel(chart);
+        pnlChart.removeAll();
+        pnlChart.add(chartpanel, BorderLayout.CENTER);
+        pnlChart.validate();
+    }
+    
+    //Nạp dữ liệu thống kê theo ngày đén lưu đồ
+    private void NapDuLieuThongKeTheoNgayDenLuuDo() throws ClassNotFoundException, SQLException
+    {
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String ToDate = format.format(Calendar.getInstance().getTime());
+        
+        Calendar TempDate = Calendar.getInstance();
+        TempDate.add(Calendar.DAY_OF_MONTH, -10);
+        String FromDate = format.format(TempDate.getTime());
+        
+        String query = "select tk.ngayxuat, count(*) as soluong from" + 
+                        " (select hoadon.ngayxuat as ngayxuat from chitiethd" + 
+                        " join hoadon on chitiethd.idhd = hoadon.idhd) tk where tk.ngayxuat between '" + FromDate +"' and '" + ToDate +"' group by tk.ngayxuat;";
+        
+        DataTable dt = new DataTable("localhost", "clock", 2, query);
+        
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        
+        //Đưa dữ liệu vào lưu đồ
+        for(int i = 0; i < dt.getRowCount(); i++)
+        {
+            //Nhận Dữ liệu từ bảng dữ liệu
+            Object row[] = dt.getRow(i);
+            Number n = new Long((long)row[1]);
+            Date date = (Date)row[0];
+            
+            //Định dạng lại ngày tháng hiển thị
+            format = new SimpleDateFormat("dd/MM/yyyy");
+            String show = format.format(date);
+            
+            dataset.addValue(n, "soluong", show);
+        }
+        
+        //Khởi tạo lưu đồ
+        JFreeChart chart = ChartFactory.createBarChart("Biểu Đồ Doanh Thu", "", "", dataset, PlotOrientation.VERTICAL, false, false, false);
+        CategoryPlot catPlot = chart.getCategoryPlot();
+        catPlot.setRangeGridlinePaint(Color.BLACK);
+        
+        //Đưa lưu đồ đến panel để hiển thị
+        ChartPanel chartpanel = new ChartPanel(chart);
+        pnlChart.removeAll();
+        pnlChart.add(chartpanel, BorderLayout.CENTER);
+        pnlChart.validate();
+    }
+    
+    //Nạp dữ liệu thống kê theo tháng đến lưu đồ
+    private void NapDuLieuThongKeTheoThangDenLuuDo() throws ClassNotFoundException, SQLException
+    {
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String ToDate = format.format(Calendar.getInstance().getTime());
+        
+        Calendar TempDate = Calendar.getInstance();
+        TempDate.add(Calendar.MONTH, -12);
+        TempDate.set(Calendar.DAY_OF_MONTH, 1);
+        String FromDate = format.format(TempDate.getTime());
+        
+        String query = "select tk.ngayxuat, count(*) as soluong from" + 
+                        " (select hoadon.ngayxuat as ngayxuat from chitiethd" + 
+                        " join hoadon on chitiethd.idhd = hoadon.idhd) tk where tk.ngayxuat between '" + FromDate +"' and '" + ToDate +"' group by tk.ngayxuat;";
+        
+        DataTable dt = new DataTable("localhost", "clock", 2, query);
+        
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        
+        for(int i = 0; i < dt.getRowCount(); i++)
+        {
+            Object row[] = dt.getRow(i);
+            Number n = new Long((long)row[1]);
+            Date date = (Date)row[0];
+            
+            //Định dạng lại tháng hiển thị
+            format = new SimpleDateFormat("MM/yyyy");
+            String show = format.format(date);
+            
+            dataset.addValue(n, "soluong", show);
+        }
+        
+        //Khởi tạo lưu đồ
+        JFreeChart chart = ChartFactory.createBarChart("Biểu Đồ Doanh Thu", "", "", dataset, PlotOrientation.VERTICAL, false, false, false);
+        CategoryPlot catPlot = chart.getCategoryPlot();
+        catPlot.setRangeGridlinePaint(Color.BLACK);
+        
+        //Đưa lưu đồ đến panel và hiển thị
+        ChartPanel chartpanel = new ChartPanel(chart);
+        pnlChart.removeAll();
+        pnlChart.add(chartpanel, BorderLayout.CENTER);
+        pnlChart.validate();
+    }
+    
     
     /**
      * This method is called from within the constructor to initialize the form.
@@ -115,8 +262,9 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
         tblDoanhSo = new javax.swing.JTable();
         jLabel9 = new javax.swing.JLabel();
         cmbDate = new javax.swing.JComboBox<>();
-        jPanel1 = new javax.swing.JPanel();
+        pnlChart = new javax.swing.JPanel();
         cmbDateDetail = new javax.swing.JComboBox<>();
+        jLabel10 = new javax.swing.JLabel();
 
         setMaximumSize(new java.awt.Dimension(741, 550));
         setMinimumSize(new java.awt.Dimension(741, 550));
@@ -175,19 +323,9 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(85, 169, 150));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Biểu đồ doanh thu", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 14))); // NOI18N
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 219, Short.MAX_VALUE)
-        );
+        pnlChart.setBackground(new java.awt.Color(85, 169, 150));
+        pnlChart.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Biểu đồ doanh thu", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 14))); // NOI18N
+        pnlChart.setLayout(new java.awt.BorderLayout());
 
         cmbDateDetail.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Theo Ngày", "Theo Tháng", "Theo Năm" }));
         cmbDateDetail.addItemListener(new java.awt.event.ItemListener() {
@@ -196,22 +334,26 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
             }
         });
 
+        jLabel10.setText("Xem Chi Tiết");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(172, 172, 172)
+                .addGap(129, 129, 129)
                 .addComponent(jLabel9)
                 .addGap(18, 18, 18)
                 .addComponent(cmbDate, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(56, 56, 56)
+                .addComponent(jLabel10)
                 .addGap(18, 18, 18)
                 .addComponent(cmbDateDetail, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(24, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(pnlChart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(43, 43, 43))
         );
@@ -222,15 +364,19 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel9)
                     .addComponent(cmbDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(cmbDateDetail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cmbDateDetail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel10))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(65, Short.MAX_VALUE))
+                .addComponent(pnlChart, javax.swing.GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
+                .addContainerGap())
         );
+
+        pnlChart.getAccessibleContext().setAccessibleName("");
     }// </editor-fold>//GEN-END:initComponents
 
+    //Cập nhật bảng hiển thị và lưu đồ thống kê khi chọn kiểu xem
     private void cmbDateItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbDateItemStateChanged
         // TODO add your handling code here:
         int index = cmbDate.getSelectedIndex();
@@ -251,6 +397,14 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
                     cmbDateDetail.addItem(date);
                     
                     current.add(Calendar.DAY_OF_MONTH, -1);
+                    
+                    try {
+                        NapDuLieuThongKeTheoNgayDenLuuDo();
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }   break;
             case 1:
                 for(int i = 0; i < 12; i++)
@@ -261,6 +415,15 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
                     cmbDateDetail.addItem(date);
                     
                     current.add(Calendar.MONTH, -1);
+                    
+                    try {
+                        NapDuLieuThongKeTheoThangDenLuuDo();
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
                 }   break;
             default:
                 int year = current.get(Calendar.YEAR);
@@ -270,11 +433,23 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
                     String date = String.valueOf(year);
                     cmbDateDetail.addItem(date);
                     year -= 1;
-                }   break;
+                }   
+
+                try 
+                {
+                    NapDuLieuThongKeTheoNamDenLuuDo();
+                } catch (ClassNotFoundException ex) 
+                {
+                    Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) 
+                {
+                    Logger.getLogger(PanelThongKeDoanhSo.class.getName()).log(Level.SEVERE, null, ex);
+                } break;
         }
         
     }//GEN-LAST:event_cmbDateItemStateChanged
 
+    //Cập nhật bảng hiển thị khi chọn chi tiết xem
     private void cmbDateDetailItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbDateDetailItemStateChanged
         // TODO add your handling code here:
         
@@ -339,10 +514,11 @@ public class PanelThongKeDoanhSo extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cmbDate;
     private javax.swing.JComboBox<String> cmbDateDetail;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel pnlChart;
     private javax.swing.JTable tblDoanhSo;
     // End of variables declaration//GEN-END:variables
 }
